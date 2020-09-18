b7b7a513fa51d6f12cbad83d81d6ef9077ce774e
Smit1400 <46996928+Smit1400@users.noreply.github.com>
2020-09-18 12:53:09 +0530

    bmi calculator ui done

--- custom_widget/lib/custom_widget.dart
+++ custom_widget/lib/custom_widget.dart
@@ -2,26 +2,20 @@ import 'package:flutter/material.dart';
 
 class CustomWidget extends StatelessWidget {
   final Color color;
-  final Icon icon;
-  CustomWidget({this.color, this.icon});
+  final Widget child;
+  CustomWidget({this.color, this.child});
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-          child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(
-            width: MediaQuery.of(context).size.width / 2   -  20,
-            height: 150,
-            decoration: BoxDecoration(
-              color: color,
-              borderRadius: BorderRadius.circular(15)
-            ),
-            child: Center(
-              child:icon
-            ),
-          ),
-        ),
+    return Container(
+      margin: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(15)
+      ),
+      child: Center(
+        child:child
+      ),
     );
   }
 }
--- custom_widget/lib/main.dart
+++ custom_widget/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:custom_widget/custom_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -11,85 +12,115 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.indigo[400],
       appBar: AppBar(
-        title: Text('Custom Widget'),
+        backgroundColor: Colors.indigo[400],
+        title: Text('BMI Calculator'),
       ),
       body: Padding(
-        padding: EdgeInsets.all(10),
-        child: CustomScrollView(
-          slivers: [
-            SliverFillRemaining(
-              hasScrollBody: false,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Row(
-                    children: [
-                      CustomWidget(
-                        color: Colors.blueGrey,
-                        icon:
-                            Icon(Icons.bookmark, size: 25, color: Colors.amber),
+          padding: EdgeInsets.all(10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: CustomWidget(
+                        color: Colors.indigo[700],
+                        child: IconWidget(
+                          icon: Icon(
+                            FontAwesomeIcons.male,
+                            size: 50,
+                            color: Colors.white,
+                          ),
+                          text: Text(
+                            'Male',
+                            style: TextStyle(color: Colors.white, fontSize: 30),
+                          ),
+                        ),
                       ),
-                      CustomWidget(
-                        color: Colors.blueGrey,
-                        icon:
-                            Icon(Icons.bookmark, size: 25, color: Colors.amber),
-                      )
-                    ],
-                  ),
-                  CustomWidget(
-                    color: Colors.blueGrey,
-                    icon: Icon(Icons.bookmark, size: 25, color: Colors.amber),
-                  ),
-                  Row(
-                    children: [
-                      CustomWidget(
-                        color: Colors.blueGrey,
-                        icon:
-                            Icon(Icons.bookmark, size: 25, color: Colors.amber),
+                    ),
+                    Expanded(
+                      child: CustomWidget(
+                        color: Colors.indigo,
+                        child: IconWidget(
+                          icon: Icon(
+                            FontAwesomeIcons.female,
+                            size: 50,
+                            color: Colors.white,
+                          ),
+                          text: Text(
+                            'Female',
+                            style: TextStyle(color: Colors.white, fontSize: 30),
+                          ),
+                        ),
                       ),
-                      CustomWidget(
-                        color: Colors.blueGrey,
-                        icon:
-                            Icon(Icons.bookmark, size: 25, color: Colors.amber),
-                      )
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      CustomWidget(
-                        color: Colors.blueGrey,
-                        icon:
-                            Icon(Icons.bookmark, size: 25, color: Colors.amber),
-                      ),
-                      CustomWidget(
-                        color: Colors.blueGrey,
-                        icon:
-                            Icon(Icons.bookmark, size: 25, color: Colors.amber),
-                      )
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      CustomWidget(
-                        color: Colors.blueGrey,
-                        icon:
-                            Icon(Icons.bookmark, size: 25, color: Colors.amber),
-                      ),
-                      CustomWidget(
-                        color: Colors.blueGrey,
-                        icon:
-                            Icon(Icons.bookmark, size: 25, color: Colors.amber),
-                      )
-                    ],
-                  ),
-                ],
+                    )
+                  ],
+                ),
               ),
-            )
-          ],
+              Expanded(
+                child: CustomWidget(
+                  color: Colors.indigo,
+                  child: Icon(Icons.bookmark, size: 25, color: Colors.amber),
+                ),
+              ),
+              Expanded(
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: CustomWidget(
+                        color: Colors.indigo,
+                        child:
+                            Icon(Icons.bookmark, size: 25, color: Colors.amber),
+                      ),
+                    ),
+                    Expanded(
+                      child: CustomWidget(
+                        color: Colors.indigo,
+                        child:
+                            Icon(Icons.bookmark, size: 25, color: Colors.amber),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          )),
+      bottomNavigationBar: SizedBox(
+        height: 70,
+        child: FlatButton(
+          onPressed: () {},
+          color: Colors.red[300],
+          child: Text(
+            'Calculate',
+            style: TextStyle(
+                color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold),
+          ),
         ),
       ),
+    );
+  }
+}
+
+class IconWidget extends StatelessWidget {
+  final Text text;
+  final Icon icon;
+
+  IconWidget({Key key, this.text, this.icon});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        icon,
+        SizedBox(
+          height: 5,
+        ),
+        text
+      ],
     );
   }
 }
